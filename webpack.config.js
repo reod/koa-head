007473716c2d5dce3d9a68477056f84b3c5b53ba
Remek Ambroziak <remek.ambroziak@gmail.com>
2018-12-05 16:33:50 +0100

    Add build process

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index.mjs',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
